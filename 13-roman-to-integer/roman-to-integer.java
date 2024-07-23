be24bedde2b83d05d62a34c0955a2948a0081cca
manjushree <111616856+mita290@users.noreply.github.com>
2024-07-23 11:10:12 +0530

    Time: 2 ms (100.00%) | Memory: 44.7 MB (43.48%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,22 +1,76 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> m = new HashMap<>();
-
-        m.put('I', 1);
-        m.put('V', 5);
-        m.put('X', 10);
-        m.put('L', 50);
-        m.put('C', 100);
-        m.put('D', 500);
-        m.put('M', 1000);
-        int n = 0;
+        int sum = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
-                n -= m.get(s.charAt(i));
-            } else {
-                n += m.get(s.charAt(i));
+            switch (s.charAt(i)) {
+                case 'M':
+                    sum += 1000;
+                    break;
+                case 'D':
+                    sum  += 500;
+                    break;
+                case 'V':
+                    sum += 5;
+                    break;
+                case 'L':
+                    sum += 50;
+                    break;
+                case 'I':
+                    if(i != s.length() - 1) {
+                        if(s.charAt(i + 1) == 'X') {
+                            i++;
+                            sum += 9;
+                            break;
+                        } else if (s.charAt(i + 1) == 'V') {
+                            i++;
+                            sum += 4;
+                            break;
+                        } else {
+                            sum+=1;
+                            break;
+                        }
+                    } else {
+                        sum += 1;
+                        break;
+                    }
+                case 'X':
+                    if(i != s.length() - 1) {
+                        if(s.charAt(i + 1) == 'L') {
+                            i++;
+                            sum += 40;
+                            break;
+                        } else if (s.charAt(i + 1) == 'C') {
+                            i++;
+                            sum += 90;
+                            break;
+                        } else {
+                            sum+=10;
+                            break;
+                        }
+                    } else {
+                        sum += 10;
+                        break;
+                    }
+                case 'C':
+                    if(i != s.length() - 1) {
+                        if(s.charAt(i + 1) == 'D') {
+                            i++;
+                            sum += 400;
+                            break;
+                        } else if (s.charAt(i + 1) == 'M') {
+                            i++;
+                            sum += 900;
+                            break;
+                        } else {
+                            sum+=100;
+                            break;
+                        }
+                    } else {
+                        sum += 100;
+                        break;
+                    }
             }
-        }  
-        return n;      
+        }
+        return sum;     
     }
 }
