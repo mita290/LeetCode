5593b4616f64fad99c8bedf87dee05b5dee5701b
manjushree <111616856+mita290@users.noreply.github.com>
2024-12-26 22:58:16 +0530

    Time: 8 ms (88.86%) | Memory: 44.3 MB (30.79%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,10 +1,18 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n <= 0) {
-            return false;
-        } else if (n == 1) {
-            return true;
-        } 
-        return n % 3 == 0 && isPowerOfThree(n / 3);
+        // using recursion
+        // if (n <= 0) {
+        //     return false;
+        // } else if (n == 1) {
+        //     return true;
+        // } 
+        // return n % 3 == 0 && isPowerOfThree(n / 3);
+
+        //mathematical property
+        // 1162261467 is 3^19,  3^20 is bigger than int  
+        return (n > 0 && Math.pow(3, 19) % n == 0);
+
+
+
     }
 }
