86ec6da6d77e9b9e42f35b1c1ddc2238f9001b42
manjushree <111616856+mita290@users.noreply.github.com>
2024-12-27 21:22:58 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (32.45%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -9,25 +9,42 @@
  * }
  */
 class Solution {
+    public int carry = 0;
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode temp = new ListNode(0);
-        ListNode tail = temp;
-        int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            int digi1 = (l1 != null) ? l1.val : 0;
-            int digi2 = (l2 != null) ? l2.val : 0;
+        // iterative
+        // ListNode temp = new ListNode(0);
+        // ListNode tail = temp;
+        // int carry = 0;
+        // while (l1 != null || l2 != null || carry != 0) {
+        //     int digi1 = (l1 != null) ? l1.val : 0;
+        //     int digi2 = (l2 != null) ? l2.val : 0;
 
-            int sum = digi1 + digi2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
+        //     int sum = digi1 + digi2 + carry;
+        //     int digit = sum % 10;
+        //     carry = sum / 10;
 
-            ListNode newNode = new ListNode(digit);
-            tail.next = newNode;
-            tail = tail.next;
-            l1 = (l1 != null) ? l1.next : null;
-            l2 = (l2 != null) ? l2.next : null;
+        //     ListNode newNode = new ListNode(digit);
+        //     tail.next = newNode;
+        //     tail = tail.next;
+        //     l1 = (l1 != null) ? l1.next : null;
+        //     l2 = (l2 != null) ? l2.next : null;
+        // }
+        // ListNode res = temp.next;
+        // return res;
+
+        // recursive
+        return solve(l1, l2);
+
+
+    }
+    public ListNode solve(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
         }
-        ListNode res = temp.next;
-        return res;
+        int sum = ((l1 != null) ? l1.val : 0) + ((l2 != null) ? l2.val : 0) + carry;
+        carry = sum / 10;
+        ListNode newNode = new ListNode(sum % 10);
+        newNode.next = solve((l1 != null) ? l1.next : null, (l2 != null) ? l2.next : l2);
+        return newNode;
     }
 }
