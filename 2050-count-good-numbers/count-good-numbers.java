2d3ee16a81735e8a07ac41e1554ce954d367bde9
manjushree <111616856+mita290@users.noreply.github.com>
2024-12-27 20:41:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (20.24%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -1,25 +1,25 @@
 class Solution {
     int mod = (int) 1e9 + 7;
     public int countGoodNumbers(long n) {
-        long first = (n % 2 == 0) ? n / 2 : ((n / 2) + 1);
+        long first = (n % 2 == 0 ? (n / 2) : (n / 2) + 1);
         long second = n / 2;
+        long even = pow(5, first) % mod;
         long odd = pow(4, second);
-        long even = pow(5, first);
         long res = 1;
         res = (res * even) % mod;
         res = (second != 0) ? (res * odd) : res;
-        return (int) (res % mod);
+        return (int)(res % mod);
     }
-
     public long pow(long x, long y) {
+        long temp;
         if (y == 0) {
-            return 1;
+            return 1; // base case: x ^ 0 = 1
         }
-        long temp = pow(x, y / 2);
+        temp = pow(x, y / 2); // temp = x ^ (y / 2)
         if (y % 2 == 0) {
-            return (temp * temp) % mod;
+            return (temp * temp) % mod; // if y is even then (x ^ y) = (x ^ (y / 2)) ^ 2 mod (10 ^ 9 + 7)
         } else {
-            return (x * temp * temp) % mod;
+            return (x * temp * temp) % mod; // if y is odd (2a + 1) then (x ^ y) = (x * (x ^ (y / 2)) ^ 2) mod (10 ^ 9 + 7)
         }
     }
 }
