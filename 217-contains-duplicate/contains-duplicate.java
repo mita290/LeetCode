c1193f844de35b4e07e5de241f813e3f6cbdd559
manjushree <111616856+mita290@users.noreply.github.com>
2025-01-01 21:58:56 +0530

    Time: 61 ms (5.11%) | Memory: 59.2 MB (15.42%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -10,13 +10,20 @@ class Solution {
         // return false;
 
         // set approach
+        // Set<Integer> ts = new TreeSet<>();
+        // for (int n : nums) {
+        //     if (ts.contains(n)) {
+        //         return true;
+        //     }
+        //     ts.add(n);
+        // }
+        // return false;
+
+        // comparing set lengths
         Set<Integer> ts = new TreeSet<>();
         for (int n : nums) {
-            if (ts.contains(n)) {
-                return true;
-            }
             ts.add(n);
         }
-        return false;
+        return ts.size() != nums.length;
     }
 }
