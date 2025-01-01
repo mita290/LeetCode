5602cdd83934441e86a1746892c74ef040cf229f
manjushree <111616856+mita290@users.noreply.github.com>
2025-01-01 22:02:51 +0530

    Time: 21 ms (9.51%) | Memory: 55.1 MB (90.45%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         // using sorting
-        // Arrays.sort(nums);
-        // for (int i = 0; i < nums.length - 1; i++) {
-        //     if (nums[i] == nums[i + 1]) {
-        //         return true;
-        //     }
-        // }
-        // return false;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
 
         // set approach
         // Set<Integer> ts = new TreeSet<>();
@@ -20,10 +20,12 @@ class Solution {
         // return false;
 
         // comparing set lengths
-        Set<Integer> ts = new TreeSet<>();
-        for (int n : nums) {
-            ts.add(n);
-        }
-        return ts.size() != nums.length;
+        // Set<Integer> ts = new TreeSet<>();
+        // for (int n : nums) {
+        //     ts.add(n);
+        // }
+        // return ts.size() != nums.length;
+
+    
     }
 }
