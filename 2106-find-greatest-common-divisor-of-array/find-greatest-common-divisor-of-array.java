3a7ad24782f3f73c4ff5335deab2dfd4b74b92ee
manjushree <111616856+mita290@users.noreply.github.com>
2025-01-14 19:41:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (6.97%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -1,28 +1,29 @@
 class Solution {
     public int findGCD(int[] nums) {
-        int max=Math.max(nums[0], nums[1]);
-        int min=Math.min(nums[0], nums[1]);
-        for(int i=2;i<=nums.length-1;i++){
-            max = Math.max(max, nums[i]);
+        int min = Math.min(nums[0], nums[1]);
+        int max = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
             min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
         }
-        int ans = 0;
-        int i=1;
-        while(i<=min){
-            if(max%i==0 && min%i==0){
-                ans = i;
+        int res = 0;
+        int i = 1;
+        while (i <= min) {
+            if (max % i == 0 && min % i == 0) {
+                res = i;
             }
             i++;
         }
-        return ans;
+        return res;
     }
 
-    public int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
+    // different implementations of euclid's method
+    // public int gcd(int a, int b) {
+    //     if (b == 0) {
+    //         return a;
+    //     }
+    //     return gcd(b, a % b);
+    // }
     // public int gcd(int a, int b) {
     //     if (a == 0) {
     //         return b;
