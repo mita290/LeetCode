1f8e58bd96da4b6535a7264f6af6c3e1b1a03997
manjushree <111616856+mita290@users.noreply.github.com>
2025-01-21 14:18:35 +0530

    Time: 47 ms (98.58%) | Memory: 49.3 MB (57.95%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,18 +1,37 @@
 class Solution {
     public int countPrimes(int n) {
-        boolean arr[] = new boolean[n + 1];
-        int c = 0;
-        for (int i = 2; i <= n; i++) {
-            arr[i] = true;
+        if (n < 3) {
+            return 0;
         }
-        for (int i = 2; i < n; i++) {
-            if (arr[i] == true) {
-                c++;
-                for (int j = i * 2; j < n; j += i) {
-                    arr[j] = false;
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime, true);
+        int res = n / 2;
+        for (int i = 3; i * i < n; i += 2) {
+            if (prime[i] == true) {
+                int x = i * 2;
+                for (int j = i * i; j < n; j += x) {
+                    if (prime[j] == true) {
+                        prime[j] = false;
+                        res--;
+                    }
                 }
             }
         }
-        return c;
+        return res;
+        // sieve of eratosthenes
+        // boolean arr[] = new boolean[n + 1];
+        // int c = 0;
+        // for (int i = 2; i <= n; i++) {
+        //     arr[i] = true;
+        // }
+        // for (int i = 2; i < n; i++) {
+        //     if (arr[i] == true) {
+        //         c++;
+        //         for (int j = i * 2; j < n; j += i) {
+        //             arr[j] = false;
+        //         }
+        //     }
+        // }
+        // return c;
     }
 }
