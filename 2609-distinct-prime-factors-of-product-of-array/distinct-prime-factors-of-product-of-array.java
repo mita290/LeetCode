38195bb983c0a8f197597630965e534c9a95a409
manjushree <111616856+mita290@users.noreply.github.com>
2025-01-24 14:53:02 +0530

    Time: 11 ms (70.69%) | Memory: 45.2 MB (48.28%) - LeetSync

---
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    public int distinctPrimeFactors(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            findPrimeFactors(num, set);
+        }
+        return set.size();
+    }
+
+    public void findPrimeFactors(int n, Set<Integer> set) {
+        // Check for factor 2
+        while (n % 2 == 0) {
+            set.add(2);
+            n /= 2;
+        }
+        // Check for odd factors starting from 3
+        for (int i = 3; i * i <= n; i += 2) {
+            while (n % i == 0) {
+                set.add(i);
+                n /= i;
+            }
+        }
+        // If n is a prime number greater than 2
+        if (n > 2) {
+            set.add(n);
+        }
+    }
+}
