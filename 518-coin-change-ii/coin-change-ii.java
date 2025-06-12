ed9b50b2baa3221e3dc44ff3b8637f6109f4aac3
manjushree <111616856+mita290@users.noreply.github.com>
2025-06-12 19:10:51 +0530

    Time: 519 ms (5.01%) | Memory: 155.8 MB (5.16%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,31 +1,24 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        // Memoization table: Map of (i, x) -> number of ways
         Map<String, Integer> memo = new HashMap<>();
-        return numWays(0, amount, coins, memo);
+        return count(0, amount, coins, memo);
     }
-
-    private int numWays(int i, int x, int[] coins, Map<String, Integer> memo) {
+    public int count(int i, int x, int[] coins, Map<String, Integer> memo) {
         String key = i + "," + x;
-
         if (memo.containsKey(key)) {
             return memo.get(key);
         }
 
-        // If we've gone through all coins
         if (i == coins.length) {
-            return x == 0 ? 1 : 0;
+            return (x == 0) ? 1 : 0;
         }
-
         int take = 0;
         if (x >= coins[i]) {
-            take = numWays(i, x - coins[i], coins, memo); // use coin[i]
+            take = count(i, x - coins[i], coins, memo);
         }
-
-        int skip = numWays(i + 1, x, coins, memo); // move to next coin
-
-        int totalWays = take + skip;
-        memo.put(key, totalWays);
-        return totalWays;
+        int skip = count(i + 1, x, coins, memo);
+        int total = take + skip;
+        memo.put(key, total);
+        return total;
     }
 }
