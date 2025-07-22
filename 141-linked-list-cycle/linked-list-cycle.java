d3f37420a9ae9aade32214d4c50ae93e72bfeaf7
manjushree <111616856+mita290@users.noreply.github.com>
2025-07-22 12:49:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (18.09%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,16 +11,16 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-            ListNode a = head;
-            ListNode b = head;
-            while (a != null && a.next != null) {
-                a = a.next.next;
-                b = b.next;
-                if (a == b) {
-                    return true;
-                }
+        ListNode a = head;
+        ListNode b = head;
+        while (a != null && a.next != null) {
+            a = a.next.next;
+            b = b.next;
+            if (a == b) {
+                return true;
             }
-            return false;
+        }
+        return false;
     }
     
 }
