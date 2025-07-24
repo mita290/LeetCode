64705f8450da54ef8519004f08633e4f55735b79
manjushree <111616856+mita290@users.noreply.github.com>
2025-07-24 14:49:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (28.33%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,42 +9,18 @@
  * }
  */
 class Solution {
-    public static ListNode tail;
-    public static boolean flag;
     public ListNode reverseList(ListNode head) {
-        // if (head == null || head.next == null) {
-        //     return head;
-        // }
-        // ListNode curr = head;
-        // ListNode nextn = head.next;
-        // ListNode prev = null;
-
-        // while (nextn != null) {
-        //     curr.next = prev;
-        //     prev = curr;
-        //     curr = nextn;
-        //     nextn = nextn.next;
-        // }
-        // curr.next = prev;
-        // return curr;
-        
         if (head == null || head.next == null) return head;
-        tail = head;
-        flag = false;
-        helper(head);
-        return head;
-
-    }
-    public static void helper(ListNode list) {
-        if (list == null ) return;
-        helper(list.next);
-
-        if (flag == true) return;
-
-        int n = tail.val;
-        tail.val = list.val;
-        list.val = n;
-        tail = tail.next;
-        if (tail == list || tail.next == list) flag = true;
+        ListNode curr = head;
+        ListNode nextn = head.next;
+        ListNode prev = null;
+        while (nextn != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = nextn;
+            nextn = nextn.next;
+        }
+        curr.next = prev;
+        return curr;
     }
 }
