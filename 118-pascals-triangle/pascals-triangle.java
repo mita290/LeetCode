80df1d92ca0651918bc7e6bef731f77d4bb3f15f
manjushree <111616856+mita290@users.noreply.github.com>
2025-10-13 13:56:27 +0530

    Time: 1 ms (85.72%) | Memory: 41.7 MB (98.69%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -8,15 +8,13 @@ class Solution {
             List<List<Integer>> res = new ArrayList<>();
             res.add(Arrays.asList(1));
             return res;
-        } 
-
+        }
         List<List<Integer>> prev = fun(n - 1);
         List<Integer> newrow = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             newrow.add(1);
         }
-        for (int i = 1; i < n - 1; i++) {
+        for  (int i = 1; i < n - 1; i++) {
             newrow.set(i, prev.get(n - 2).get(i - 1) + prev.get(n - 2).get(i));
         }
         prev.add(newrow);
