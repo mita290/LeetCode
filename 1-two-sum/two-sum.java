15445c9bf1dc824a0b45c659b7c63bdabbedbdd4
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-20 14:37:40 +0530

    Time: 3 ms (59.22%) | Memory: 47 MB (59.69%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,11 +1,37 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (target == nums[i] + nums[j]) {
-                    return new int[]{i, j};
-                }
+        // brute force
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return new int[]{i, j};
+        //         }
+        //     }
+        // }
+        // return new int[]{};
+
+        // two pointers - doesn't work for this problem as the array isn't sorted, and if you sort it the indices will change
+
+        // int l = 0, r = nums.length - 1;
+        // Arrays.sort(nums);
+        // int i = 0;
+        // while (l < r) {
+        //     int res = nums[l] + nums[r];
+        //     if (res == target) return new int[]{l, r};
+        //     else if (res < target) ++l;
+        //     else --r;
+        // }
+        // return new int[]{};
+
+        // optimal solution using hashing
+        
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (hm.containsKey(x)) {
+                return new int[]{hm.get(x), i};
             }
+            hm.put(nums[i], i);
         }
         return new int[]{};
     }
