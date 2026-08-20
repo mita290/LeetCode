ba88de4f60ee595673f235db5a1f7d1126cbff2a
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-20 22:41:33 +0530

    Time: 1 ms (99.89%) | Memory: 45.8 MB (13.49%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,26 +1,51 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        HashMap<Integer, Integer> hm = new LinkedHashMap<>();
-        ArrayList<Integer> al = new ArrayList<>();
+
+        int[] allNums = new int[101];
+        int[] res = new int[nums.length];
+
         for (int i = 0; i < nums.length; i++) {
-            if (hm.containsKey(nums[i])) {
-                continue;
+            allNums[nums[i]]++;
+        }
+
+        for (int i = 1; i < 101; i++) {
+            allNums[i] += allNums[i - 1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                res[i] = 0;
             } else {
-                hm.put(nums[i], 0);
-                for (int j = 0; j < nums.length; j++) {
-                    if (nums[j] < nums[i]) {
-                        hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
-                    }
-                }
+                res[i] = allNums[nums[i] - 1];
             }
         }
-        // return hm.values().stream().mapToInt(i -> i).toArray(); --> doesn't work if numbers are included multiple times but useful syntax
 
-        for (int n : nums) {
-            al.add(hm.get(n));
-        }
-        return al.stream().mapToInt(i -> i).toArray();
-        // alt --> return al.stream().mapToInt(Integer::intValue).toArray();
+        return res;
+
+
+
+
+        // HashMap<Integer, Integer> hm = new LinkedHashMap<>();
+        // ArrayList<Integer> al = new ArrayList<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (hm.containsKey(nums[i])) {
+        //         continue;
+        //     } else {
+        //         hm.put(nums[i], 0);
+        //         for (int j = 0; j < nums.length; j++) {
+        //             if (nums[j] < nums[i]) {
+        //                 hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        //             }
+        //         }
+        //     }
+        // }
+        // // return hm.values().stream().mapToInt(i -> i).toArray(); --> doesn't work if numbers are included multiple times but useful syntax
+
+        // for (int n : nums) {
+        //     al.add(hm.get(n));
+        // }
+        // return al.stream().mapToInt(i -> i).toArray();
+        // // alt --> return al.stream().mapToInt(Integer::intValue).toArray();
 
     }
 }
