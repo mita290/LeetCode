4cf715b08db1854bf279ae139e833e1de4172248
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-20 22:06:55 +0530

    Time: 6 ms (83.94%) | Memory: 67 MB (74.12%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,19 +1,37 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
 
-        HashSet<Integer> hs = new HashSet<>();
         ArrayList<Integer> al = new ArrayList<>();
-
-        for (int n : nums) {
-            hs.add(n);
-        }
         
-        for (int i = 1; i <= nums.length; i++) {
-            if (!hs.contains(i)) {
-                al.add(i);
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] *= -1;
             }
-        }        
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                al.add(i + 1);
+            }
+        }
+
         return al;
+
+        // not the most optimal
+        // HashSet<Integer> hs = new HashSet<>();
+        // ArrayList<Integer> al = new ArrayList<>();
+
+        // for (int n : nums) {
+        //     hs.add(n);
+        // }
+        
+        // for (int i = 1; i <= nums.length; i++) {
+        //     if (!hs.contains(i)) {
+        //         al.add(i);
+        //     }
+        // }        
+        // return al;
 
         // only works for 30/35 testcases
         // HashMap<Integer, Boolean> hm = new HashMap<>();
