f618c1ed0fe715b66b51f004be70e603997ce138
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-20 14:07:43 +0530

    Time: 3 ms (94.22%) | Memory: 83.1 MB (36.80%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,23 +1,34 @@
 class Solution {
     public int findDuplicate(int[] arr) {
 
-        //using hashset
-        HashSet<Integer> set = new HashSet<>();
-        int len = arr.length;
-        for (int i = 0; i < len; i++) {
-            if (!set.add(arr[i])) {
-                return arr[i];
+        // marking visited values within an array
+        for (int n : arr) {
+            int x = Math.abs(n);
+            if (arr[x] < 0) {
+                return x;
             }
-        }
-
-        // using hashmap
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (hm.containsKey(arr[i]) && hm.get(arr[i]) > 0) {
-                return arr[i];
-            }
-            hm.put(arr[i], 1);
+            arr[x] = -arr[x];
         }
         return -1;
+
+        //using hashset
+        // HashSet<Integer> set = new HashSet<>();
+        // int len = arr.length;
+        // for (int i = 0; i < len; i++) {
+        //     if (!set.add(arr[i])) {
+        //         return arr[i];
+        //     }
+        // }
+        // return -1
+
+        // using hashmap
+        // HashMap<Integer, Integer> hm = new HashMap<>();
+        // for (int i = 0; i < arr.length; i++) {
+        //     if (hm.containsKey(arr[i]) && hm.get(arr[i]) > 0) {
+        //         return arr[i];
+        //     }
+        //     hm.put(arr[i], 1);
+        // }
+        // return -1;
     }
 }
