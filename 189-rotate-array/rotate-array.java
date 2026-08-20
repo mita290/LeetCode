282225c731533b0ea65d0764ea7d7f2faa8e94ee
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-20 12:55:03 +0530

    Time: 4 ms (5.03%) | Memory: 268.6 MB (5.04%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,6 +1,10 @@
 class Solution {
-    public void rev(int arr[], int l, int r) {
+    public void rev(int[] arr, int l, int r) {
         while (l < r) {
+            // can sometimes cause problems -->  if l == r then the result would be 0
+            // arr[l] ^= arr[r];
+            // arr[r] ^= arr[l];
+            // arr[l++] ^= arr[r--];
             int t = arr[l];
             arr[l] = arr[r];
             arr[r] = t;
@@ -14,17 +18,5 @@ class Solution {
         rev(arr, 0, n - 1);
         rev(arr, 0, k - 1);
         rev(arr, k, n - 1);
-
-        // passes only 37/38 testcases
-        // int ct = 0, n = nums.length;
-        // k %= n;
-        // while (ct < k) {
-        //     int x = nums[n - 1];
-        //     for (int i = n - 1; i >= 1; i--) {
-        //         nums[i] = nums[i - 1];
-        //     }
-        //     nums[0] = x;
-        //     ct++;
-        // }
     }
 }
