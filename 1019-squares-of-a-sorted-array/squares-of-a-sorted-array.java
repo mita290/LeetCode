e947c4e996ca13922af1121636d5b01a22c4258d
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-21 01:50:51 +0530

    Time: 1 ms (100.00%) | Memory: 47.4 MB (43.44%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,6 +1,21 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int i = 0, j = n - 1, idx = n - 1;
+
+        while(i <= j) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                res[idx--] = nums[i] * nums[i++];
+            } else {
+                res[idx--] = nums[j] * nums[j--];
+            }
+        }        
+        return res;
+
+
+        /* using streams --> very long time
         ArrayList<Integer> al = Arrays.stream(nums).boxed().map(val -> val * val).sorted().collect(Collectors.toCollection(ArrayList::new));
-        return al.stream().mapToInt(i -> i).toArray();
+        return al.stream().mapToInt(i -> i).toArray(); */
     }
 }
