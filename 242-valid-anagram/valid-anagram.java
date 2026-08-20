7014f9c90e9c83b34de2eb1537448eaae250e88b
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-20 12:51:36 +0530

    Time: 2 ms (99.65%) | Memory: 44.5 MB (66.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,9 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        return Arrays.equals(arr1, arr2);
+        // frequency counting
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            freq[c - 'a']--;
+        }
+        for (int x : freq) {
+            if (x != 0) return false;
+        }
+        return true;
+
+        // not optimal approach --> space complexity becomes O(n) and auxilliary space has to be considered
+        // char[] arr1 = s.toCharArray();
+        // char[] arr2 = t.toCharArray();
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
+        // return Arrays.equals(arr1, arr2);
     }
 }
