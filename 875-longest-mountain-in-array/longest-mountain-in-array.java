986decab6ecc390b5856e51f0bb6209af6e3f6e3
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-21 16:46:12 +0530

    Time: 2 ms (99.68%) | Memory: 47.3 MB (53.97%) - LeetSync

---
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int longestMountain(int[] arr) {
+
+        int n = arr.length;
+        int res = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] <= arr[i - 1] || arr[i] <= arr[i + 1]) {
+                continue;
+            }
+            int left = i, right = i;
+            while (left > 0 && arr[left] > arr[left - 1]) {
+                left--;
+            }
+            while (right < n - 1 && arr[right] > arr[right + 1]) {
+                right++;
+            }
+            res = Math.max(right - left + 1,  res);
+        }
+        return res;
+
+
+
+        /* In this approach we try to solve from the outward side to the inside but that's not reliable
+        the better idea would be to check for this order strictly increasing >> peak >> strictly decreasing
+        int n = arr.length, res = 0;
+        int left = 0, right = n - 1;
+        
+        while (left < right) {
+            if ((left == right) || (right - left < 3)) break;
+
+            if (arr[left] > arr[left + 1]) {
+                left++;
+                System.out.println("Left value: " + arr[left]);
+            }
+            if (arr[right] > arr[right - 1]) {
+                right--;
+                System.out.println("Right value: " + arr[right]);
+            }
+            int l = left, r = right;
+            while (arr[l] < arr[l + 1] && arr[r] < arr[r - 1] && l != r) {
+                l++;
+                r--;
+            }
+            System.out.println("Final value of l and r: " + arr[l] + " " + arr[r]);
+            if (l == r) {
+                res = Math.max(right - left + 1, res);
+                break;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return res; */
+    }
+}
