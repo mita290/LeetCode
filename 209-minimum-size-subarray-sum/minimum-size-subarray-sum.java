13d91af36ba615f0ac3feabaf584351f8bec9b4a
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-21 19:39:59 +0530

    Time: 7 ms (2.03%) | Memory: 76.6 MB (11.47%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,9 +1,11 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
 
+        // sliding window approach
+
         Queue<Integer> q = new LinkedList<>();
         int sum = 0, i = 0, len = Integer.MAX_VALUE;
-        
+
         while (i < nums.length) {
 
             sum += nums[i];
