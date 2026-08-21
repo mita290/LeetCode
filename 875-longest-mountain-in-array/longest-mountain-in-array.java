b533ca5bbfa413b5d93028beb86bff14259e54fe
manjushree <111616856+mita290@users.noreply.github.com>
2026-08-21 16:53:12 +0530

    Time: 3 ms (72.61%) | Memory: 47.6 MB (6.68%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -3,6 +3,37 @@ class Solution {
 
         int n = arr.length;
         int res = 0;
+        int i = 1;
+
+        while (i < n - 1) {
+            
+            if (arr[i] <= arr[i - 1] || arr[i] <= arr[i + 1]) {
+                i++;
+                continue;
+            }
+
+            int left = i, right = i;
+
+            while (left > 0 && arr[left] > arr[left - 1]) {
+                left--;
+            }
+            while (right < n - 1 && arr[right] > arr[right + 1]) {
+                right++;
+            }
+
+            res = Math.max(right - left + 1,  res);
+            i = right;
+
+        }
+        return res;
+
+
+        // in the optimal approach, we identify the peak first and extend the left and right edges
+        // we repeat this process for all elements in the array
+        // we ca n optimise this even further as shown above, by making sure we don't process the same mountains again and again
+        /*
+        int n = arr.length;
+        int res = 0;
 
         for (int i = 1; i < n - 1; i++) {
             if (arr[i] <= arr[i - 1] || arr[i] <= arr[i + 1]) {
@@ -18,7 +49,7 @@ class Solution {
             res = Math.max(right - left + 1,  res);
         }
         return res;
-
+        */
 
 
         /* In this approach we try to solve from the outward side to the inside but that's not reliable
